7bb319f5a15dd17ba80e8cbd743fb491268e9efb
Nageswara Rao Gude <120391864+nag-gude@users.noreply.github.com>
2023-04-19 16:00:42 +0100

    Updated lifecycle block

--- modules/gitlab_repository_file/main.tf
+++ modules/gitlab_repository_file/main.tf
@@ -1,4 +1,4 @@
-resource "gitlab_repository_file" "repo_file" {
+resource "gitlab_repository_file" "file" {
   project             = var.project
   file_path           = var.file_path
   branch              = var.branch_name
@@ -6,13 +6,12 @@ resource "gitlab_repository_file" "repo_file" {
   author_email        = var.author_email
   author_name         = var.author_name
   commit_message      = var.commit_message
-  overwrite_on_create = var.overwrite_on_create
   execute_filemode    = var.execute_filemode
   start_branch        = var.start_branch
 
   lifecycle {
     ignore_changes = [
-      content,
+      file_name, content, author_email, author_name, commit_message
     ]
   }
 }
